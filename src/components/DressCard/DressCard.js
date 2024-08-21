2b73577cedcbb3e4fc828b6d226fa7549df05a0b
vaishnavishahakar <vaishnavishahakar1@gmail.com>
2024-08-21 21:38:15 +0530

    create DressCard.js file

---
+++ src/components/DressCard/DressCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./DressCard.css"
+
+function DressCard() {
+  return (
+    <div>
+      DressCard
+    </div>
+  );
+}
+
+export default DressCard;
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import "./Home.css"
 
+import DressCard from './../../components/DressCard/DressCard';
+
 function Home() {
   return (
     <div>
       <h1>FASHION - WARDROBE</h1>
+
+      <DressCard/>
     </div>
   );
 }
